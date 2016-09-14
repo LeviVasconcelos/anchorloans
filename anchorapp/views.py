3ccc329878b30f64ce269c144a22c08c4962d76b
Levi O Vasconcelos <leviovasconcelos@gmail.com>
2016-09-14 16:23:28 -0300

    initial version

--- anchorapp/views.py
+++ anchorapp/views.py
@@ -1,21 +1,32 @@
 from anchorapp import app, inpfiles
 from flask_uploads import UploadNotAllowed
-from flask import redirect, url_for, request, render_template
-from utils import DivisibilityProblem
+from flask import redirect, url_for, request, render_template, g
+from utils import DivisibilityProblem, BadInstance, FileValidationError
+import os
 
 @app.route('/')
 def start():
-	return render_template('base.html')
+	return render_template('base.html', input_text='Input file text', output_text='Output file text')
 
 @app.route('/upload', methods=['POST'])
 def upload():
 	if 'inpt' in request.files:
 		try:
-			filename = inpfiles.save(request.files['inpt'])
-		except UploadNotAllowed as e:
+			g.filename = inpfiles.save(request.files['inpt'])
+			path_to_file = os.path.join(os.path.dirname(__file__), 'inputs/' + g.filename)
+			g.file = open(path_to_file,'r')
+			return render_template('base.html', input_text=g.file.read(), output_text='Output file text, generate on Compute button.')
+		except UploadNotAllowed as e: #TO-DO: FLASH ERROR MESSAGE INTO THE SAME TEMPLATE.
 			return 'Sorry, this upload was not allowed, text files only.'
-	return render_template('base.html')	
+	return render_template('base.html', input_text='Input file text', output_text='Output file text')	
 
-@app.route('/loaded/<string:filename>')
-def loadfile(filename):
+@app.route('/compute')
+def compute(filename):
+	solver = DivisibilityProblem()
+	parser = FileParser(g.file)
+
+	try:
+		parser.parse()
+	except FileValidationError as e:
+		return e
 	return ''
--- t.py
+++ t.py
@@ -1,6 +1,3 @@
-import os
-import io
-
 def representInt(x):
 	try:
 		int(x)
@@ -8,49 +5,16 @@ def representInt(x):
 	except ValueError:
 		return False
 
-class BadInstance(Exception):
-	pass
-
 class FileValidationError(Exception):
     pass
 
-class DivisibilityProblem:
-	def __init__(self, max = None, x = None, y = None):
-		self.max_rage = max
-		self.x = x
-		self.y = y
-		self.seq = []
-
-	def setInstance(self, m, x, y):
-		if (m and x and y) is not None:
-			self.max_range = m
-			self.x = x
-			self.y = y
-			self.seq = []
-		else:
-			raise BadInstance('Invalid Intance.')
-
-	def _solve(self):
-		if (self.max_range and self.x and self.y) is not None:
-			self.seq = [i for i in range(self.x, self.max_range, self.x) if (i % self.y)]
-		else:
-			raise BadInstance('Invalid Intance.')
-
-	def getSolution(self):
-		self._solve(self)
-		return self.seq
-
-
-
 
 class FileParser:
-	def __init__(self, filename=None, file=None):
+	def __init__(self, filename=None):
 		self.filename = filename
 		self.file = None
-		if filename is not None and file is None:
+		if filename is not None:
 			self.file = open(filename, 'r')
-		if file is not None:
-			self.file = file
 
 		self.instance_size = None
 		self.instances = []
@@ -64,7 +28,6 @@ class FileParser:
 				if not representInt(l):
 					raise FileValidationError('Invalid file format: 1st line should be an integer.')
 				self.instance_size = int(l)
-
 			else:
 				tmp = l.split(' ')
 				params = []
@@ -76,3 +39,12 @@ class FileParser:
 					raise FileValidationError('Invalid file format: instance parameters numbers different than 3.')
 				self.instances.append(params)
 
+
+
+parser = FileParser('teste.txt')
+try:
+	parser.parse()
+	for i in parser.instances:
+		print i
+except FileValidationError as e:
+	print e
